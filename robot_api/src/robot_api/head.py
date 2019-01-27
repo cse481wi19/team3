b78636921b5528565b649013a5b316e61a350ad7
team3 <tarkucar@mac.com>
2019-01-26 18:51:51 -0800

    temp

--- robot_api/src/robot_api/head.py
+++ robot_api/src/robot_api/head.py
@@ -11,8 +11,8 @@ ACTION_SERVER2 = 'head_controller/point_head'
 
 LOOK_AT_ACTION_NAME = ''  # TODO: Get the name of the look-at action
 PAN_TILT_ACTION_NAME = ''  # TODO: Get the name of the pan/tilt action
-PAN_JOINT = ''  # TODO: Get the name of the head pan joint
-TILT_JOINT = ''  # TODO: Get the name of the head tilt joint
+PAN_JOINT = 'head_pan_joint'  # TODO: Get the name of the head pan joint
+TILT_JOINT = 'head_tilt_joint'  # TODO: Get the name of the head tilt joint
 PAN_TILT_TIME = 2.5  # How many seconds it should take to move the head.
 
 
@@ -28,10 +28,10 @@ class Head(object):
         head.look_at('base_link', 1, 0, 0.3)
         head.pan_tilt(0, math.pi/4)
     """
-    MIN_PAN = None  # TODO: Minimum pan angle, in radians.
-    MAX_PAN = None  # TODO: Maximum pan angle, in radians.
-    MIN_TILT = None  # TODO: Minimum tilt angle, in radians.
-    MAX_TILT = None  # TODO: Maximum tilt angle, in radians.
+    MIN_PAN = -math.pi/2.0  # TODO: Minimum pan angle, in radians.
+    MAX_PAN = math.pi/2.0  # TODO: Maximum pan angle, in radians.
+    MIN_TILT = -math.pi/2.0  # TODO: Minimum tilt angle, in radians.
+    MAX_TILT = math.pi/4.0  # TODO: Maximum tilt angle, in radians.
 
     def __init__(self):
         # TODO: Create actionlib clients
@@ -53,10 +53,18 @@ class Head(object):
             z: The z value of the point to look at.
         """
         # TODO: Create goal
+        goal = control_msgs.msg.FollowJointTrajectoryGoal()
         # TODO: Fill out the goal (we recommend setting min_duration to 1 second)
+        goal.trajectory.joint_names.append(TILT_JOINT)
+        point = trajectory_msgs.msg.JointTrajectoryPoint()
+        point.positions.append(x)
+        point.positions.append(y)
+        point.positions.append(z)
+        point.time_from_start = rospy.Duration(1)
         # TODO: Send the goal
+        self._client1.send_goal(goal)
         # TODO: Wait for result
-        rospy.logerr('Not implemented.')
+        self._client1.wait_for_result(rospy.Duration(10))
 
     def pan_tilt(self, pan, tilt):
         """Moves the head by setting pan/tilt angles.
