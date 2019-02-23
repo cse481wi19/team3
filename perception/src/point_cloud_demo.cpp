3e6d4d72f516d2e4d8ddd3a06bb1c24e8ac83852
team3 <tarkucar@mac.com>
2019-02-23 15:15:28 -0800

    Completed lab 32

--- perception/src/point_cloud_demo.cpp
+++ perception/src/point_cloud_demo.cpp
@@ -2,6 +2,7 @@
 #include "perception/downsample.h"
 #include "perception/segmentation.h"
 #include "sensor_msgs/PointCloud2.h"
+#include <visualization_msgs/Marker.h>
 #include "ros/ros.h"
 
 int main(int argc, char** argv) {
@@ -10,17 +11,21 @@ int main(int argc, char** argv) {
   std::string cloud_in = "mock_point_cloud";
   nh.getParam("cloud_in", cloud_in);
 
-  ros::Publisher table_pub =
-      nh.advertise<sensor_msgs::PointCloud2>("table_cloud", 1, true);
-  perception::Segmenter segmenter(table_pub);
-  ros::Subscriber sub =
-      nh.subscribe(cloud_in, 1, &perception::Segmenter::Callback, &segmenter);
-
   ros::Publisher crop_pub =
       nh.advertise<sensor_msgs::PointCloud2>("cropped_cloud", 1, true);
   perception::Cropper cropper(crop_pub);
   ros::Subscriber sub_in =
       nh.subscribe(cloud_in, 1, &perception::Cropper::Callback, &cropper);
+
+  ros::Publisher table_pub =
+      nh.advertise<sensor_msgs::PointCloud2>("table_cloud", 1, true);
+  ros::Publisher marker_pub =
+      nh.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
+  ros::Publisher above_surface_pub =
+      nh.advertise<sensor_msgs::PointCloud2>("above_surface", 1, true);
+  perception::Segmenter segmenter(table_pub, marker_pub, above_surface_pub);
+  ros::Subscriber sub =
+      nh.subscribe("cropped_cloud", 1, &perception::Segmenter::Callback, &segmenter);
 
   ros::Publisher downsample_pub =
       nh.advertise<sensor_msgs::PointCloud2>("downsampled_cloud", 1, true);
