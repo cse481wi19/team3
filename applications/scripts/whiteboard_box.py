5ba3ba40220d4b587cc733aac4a01fcf9cc844df
Tarkan Al-Kazily <tarkucar@uw.edu>
2019-03-06 17:17:38 -0800

    Added standardization to the whiteboard orientation

--- applications/scripts/whiteboard_box.py
+++ applications/scripts/whiteboard_box.py
@@ -4,7 +4,7 @@ import robot_api
 import rospy
 import tf
 import tf.transformations as tft
-from geometry_msgs.msg import Pose, Point, Quaternion
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
 from visualization_msgs.msg import MarkerArray, Marker
 import numpy as np
 import math
@@ -63,8 +63,11 @@ def main():
     whiteboard_width = 0.5
     whiteboard_height = 0.5
     whiteboard_depth = 0.1
+
+    corners = {14:"bottom left"}
+
     br = tf.TransformBroadcaster()
-    lf = tf.TransformListener()
+    tfl = tf.TransformListener()
     markers = MarkerTracker()
     sub = rospy.Subscriber('whiteboard_tags', MarkerArray,
             callback=markers.callback) # Subscribe to AR tag poses, use reader.callback
@@ -74,8 +77,9 @@ def main():
 
     pub = rospy.Publisher('whiteboard_region', Marker, queue_size=1)
     
-    r = rospy.Rate(2)
-    def makeMarker(pose):
+    r = rospy.Rate(30)
+
+    def makeMarker(orientation):
         marker = Marker()
         marker.type = Marker.CUBE
         marker.scale.x = whiteboard_width
@@ -85,13 +89,52 @@ def main():
         marker.color.g = 0.0
         marker.color.b = 0.0
         marker.color.a = 0.5
-        marker.pose.orientation = deepcopy(pose.pose.orientation)
+        marker.pose.orientation = deepcopy(orientation)
         marker.header.frame_id = 'base_link'
         return marker
+
     whiteboard_marker = makeMarker(pose)
+
+    def stabilizeOrientation(orientation):
+        def expandQuat(q):
+            return [q.x, q.y, q.z, q.w]
+
+        def rotateVectorByQuat(v, q):
+            rot = tft.quaternion_matrix(expandQuat(q))
+            return np.dot(rot, [v[0], v[1], v[2], 1])[0:3]
+
+        """Returns the quaternion obtained by rotating 'orientation' so that x
+           is perpendicular to the robot's vertical direction and in the
+           plane of the whiteboard. In this case, x is positive to the right"""
+        old_x = [1, 0, 0]
+        old_y = [0, 1, 0]
+        old_z = [0, 0, 1]
+        new_x = rotateVectorByQuat(old_x, orientation)
+        new_y = rotateVectorByQuat(old_y, orientation)
+        new_z = rotateVectorByQuat(old_z, orientation)
+        reflect = np.identity(4)
+
+        if new_z[0] < 0:
+            reflect = tft.reflection_matrix([0, 0, 0], new_z)
+
+        goal_x = tft.unit_vector(np.cross(new_z, old_z))
+        angle = np.arccos(np.dot(new_x, goal_x))
+        rot = tft.rotation_matrix(angle, new_z)
+        
+        res_x = np.dot(rot, [new_x[0], new_x[1], new_x[2], 1])[0:3]
+
+        if res_x[1] > 0:
+            rot = tft.rotation_matrix(angle + np.pi, new_z)
+
+        old_quat = expandQuat(pose.pose.orientation)
+        rot_quat = tft.quaternion_from_matrix(rot)
+        reflect_quat = tft.quaternion_from_matrix(reflect)
+        new_quat = tft.quaternion_multiply(tft.quaternion_multiply(rot_quat, old_quat), reflect_quat)
+        return Quaternion(new_quat[0], new_quat[1], new_quat[2], new_quat[3])
 
     while 1:
         r.sleep()
+        orientation = stabilizeOrientation(pose.pose.orientation)
         if len(markers.markers) == 0:
             print "No AR markers found"
             continue
@@ -99,32 +142,27 @@ def main():
         if reference_marker is None:
             print "What you do?"
             continue
-        whiteboard_marker = makeMarker(pose)
+        whiteboard_marker = makeMarker(orientation)
         whiteboard_marker.pose.position = deepcopy(reference_marker.pose.position)
-        whiteboard_frame = deepcopy(whiteboard_marker.pose)
+        whiteboard_ps = PoseStamped()
+        whiteboard_ps.pose.orientation = deepcopy(orientation)
+        whiteboard_ps.header.frame_id = "base_link"
+        
+        try:
+            whiteboard_ps = tfl.transformPose('odom', whiteboard_ps)
+        except (e):
+            continue
+
+        whiteboard_ps.pose.position = deepcopy(reference_marker.pose.position)
+
+        """Shift whiteboard marker position so bottom corner is correctly placed"""
         whiteboard_marker.pose.position.y -= whiteboard_width / 2.0
         whiteboard_marker.pose.position.z += whiteboard_height / 2.0
-
         pub.publish(whiteboard_marker)
-        converted = convertPose(whiteboard_frame, lf)
-        old_rot = [converted.orientation.x,
-            converted.orientation.y, converted.orientation.z,
-            converted.orientation.w]
-        angles = tft.euler_from_quaternion([converted.orientation.x,
-            converted.orientation.y, converted.orientation.z,
-            converted.orientation.w])
-        print("Angles:", angles)
-        rot = tft.quaternion_from_euler(0, 0, 0)
-        res_orientation = tft.quaternion_multiply(rot, old_rot)
-        converted.orientation = Quaternion(res_orientation[0], res_orientation[1], res_orientation[2], res_orientation[3])
+        
 
-        print("whiteboard:")
-        print(converted)
-        print("marker:")
-        print(reference_marker.pose)
-        print("======")
-        br.sendTransform((converted.position.x, converted.position.y, converted.position.z),
-                         (converted.orientation.x, converted.orientation.y, converted.orientation.z, converted.orientation.w),
+        br.sendTransform((whiteboard_ps.pose.position.x, whiteboard_ps.pose.position.y, whiteboard_ps.pose.position.z),
+                         (whiteboard_ps.pose.orientation.x, whiteboard_ps.pose.orientation.y, whiteboard_ps.pose.orientation.z, whiteboard_ps.pose.orientation.w),
                          rospy.Time.now(),
                          "whiteboard",
                          "odom")
