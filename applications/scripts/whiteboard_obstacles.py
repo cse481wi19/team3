4b3df3cf0cd2e1d2840dbdc482f6f73fd43189fe
Tarkan Al-Kazily <tarkucar@uw.edu>
2019-03-12 11:35:33 -0700

    Updated the planning scene with more obstacles

--- applications/scripts/whiteboard_obstacles.py
+++ applications/scripts/whiteboard_obstacles.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 from moveit_python import PlanningSceneInterface
-import robot_api
 import rospy
 import tf
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
@@ -28,7 +27,10 @@ def main():
 
     planning_scene = PlanningSceneInterface('base_link')
     planning_scene.clear()
-    planning_scene.removeCollisionObject('floor')
+    planning_scene.removeCollisionObject('wall')
+    planning_scene.removeCollisionObject('tray')
+    planning_scene.removeAttachedObject('block')
+
     box_pose = PoseStamped()
     box_pose.header.frame_id = "whiteboard"
     box_pose.pose.orientation.w = 1.0
@@ -41,10 +43,23 @@ def main():
             rospy.sleep(1)
     p = SolidPrimitive()
     p.type = SolidPrimitive.BOX
-    p.dimensions = [20.0, 3.0, 0.1]
-    planning_scene.addSolidPrimitive('floor', p, res_pose.pose)
+    p.dimensions = [10.0, 3.0, 0.1]
+    planning_scene.addSolidPrimitive('wall', p, res_pose.pose)
+    tray = SolidPrimitive()
+    tray.type = SolidPrimitive.BOX
+    tray.dimensions = [10.0, 0.85, 0.2]
+    res_pose.pose.position.z = tray.dimensions[1] / 2.0
+    res_pose.pose.position.x -= tray.dimensions[2] / 2.0
+    planning_scene.addSolidPrimitive('tray', tray, res_pose.pose)
 
-    rospy.sleep(2)
+    frame_attached_to = 'gripper_link'
+    frames_okay_to_collide_with = [
+        'gripper_link', 'l_gripper_finger_link', 'r_gripper_finger_link'        
+    ]
+    planning_scene.attachBox('block', 0.13, 0.05, 0.05, 0.0, 0.0, 0,
+            frame_attached_to, frames_okay_to_collide_with)
+
+    rospy.sleep(1)
 
 
 if __name__ == '__main__':
